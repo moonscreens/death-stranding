894a32d19d5078660b8bff014015d83f95607840
calebbabin <object.caleb@gmail.com>
2022-04-02 22:01:21 -0500

    improve rain performance

--- src/stuff/rain.js
+++ src/stuff/rain.js
@@ -1,14 +1,14 @@
 import * as THREE from 'three';
 import { drawFunctions, camera, farDistance } from '../scene';
 
-const count = 7500;
+const count = 2000;
 const maxY = farDistance * 0.25;
 
-const rainGeometry = new THREE.BoxBufferGeometry(0.03, 0.2, 0.03);
+const rainGeometry = new THREE.BoxBufferGeometry(0.05, 0.4, 0.05);
 const rainMaterial = new THREE.MeshBasicMaterial({
-	color: 0x0066CC,
+	color: 0x0033AA,
 	transparent: true,
-	opacity: 0.5,
+	opacity: 0.2,
 });
 
 const RainMesh = new THREE.InstancedMesh(rainGeometry, rainMaterial, count);
@@ -17,10 +17,17 @@ RainMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
 const dummyMatrix = new THREE.Matrix4();
 const dummy = new THREE.Object3D();
 
+function getX() {
+	return (Math.random() * 2 - 1) * farDistance;
+}
+function getZ() {
+	return (camera.position.z - farDistance * 0.05) - (farDistance * 1.25 * Math.random());
+}
 
 for (let index = 0; index < count; index++) {
-	dummy.position.x = (Math.random() * 2 - 1) * farDistance * 2;
-	dummy.position.y = (Math.random() * 2 - 1) * maxY;
+	dummy.position.x = getX();
+	dummy.position.y = (Math.random()) * maxY;
+	dummy.position.z = getZ();
 	dummy.updateMatrix();
 	RainMesh.setMatrixAt(index, dummy.matrix);
 };
@@ -30,11 +37,12 @@ drawFunctions.push(delta => {
 		RainMesh.getMatrixAt(index, dummyMatrix)
 		dummy.position.setFromMatrixPosition(dummyMatrix);
 
-		dummy.position.y -= delta * 40;
+		dummy.position.y -= delta * 35;
 
 		if (dummy.position.y < 0) {
+			dummy.position.x = getX();
 			dummy.position.y += maxY;
-			dummy.position.z = (camera.position.z - farDistance * 0.1) - (farDistance * Math.random());
+			dummy.position.z = getZ();
 		}
 		dummy.updateMatrix();
 		RainMesh.setMatrixAt(index, dummy.matrix);
