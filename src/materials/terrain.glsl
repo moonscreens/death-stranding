ab03f039281fdfd461a7c20b8343bb732668e128
calebbabin <object.caleb@gmail.com>
2022-04-15 17:33:42 -0500

    wiggle the river further away, flatten mountains near camera

--- src/materials/terrain.glsl
+++ src/materials/terrain.glsl
@@ -14,7 +14,8 @@ float difference(float a, float b) {
 
 float riverX = 5.0;
 float riverWidth = 10.0;
-float riverWiggle = 30.0;
+float riverWiggle = 60.0;
+float cameraFlattenDistance = 80.0;
 
 float getRiver(float x, float y, float width) {
 	float riverIntensity = 1.0 - difference(x, riverX + snoise(vec3(0.0, y, 0.0) * 0.005) * riverWiggle) / (riverWidth * width);
@@ -26,8 +27,8 @@ float getRiver(float x, float y, float width) {
 float getTerrainNoise(float x, float y) {
 	float riverIntensity = getRiver(x, y, 1.0);
 	return (// mountains
-	(noise1(x * 0.0014, y * 0.0014) * 20.0 * (1.0 - getRiver(x, y, 7.0))) + 
-	((noise1(x * 0.01, y * 0.01)) * 15.0 * max(0.0, min(1.0, pow((difference(x, 0.0) / 30.0), 2.0)))) * (1.0 - getRiver(x, y, 4.0)) +
+	(noise1(x * 0.0014, y * 0.0014) * 20.0 * (1.0 - getRiver(x, y, 7.0))) * min(abs(x / cameraFlattenDistance), 1.0) +
+		((noise1(x * 0.01, y * 0.01)) * 15.0 * max(0.0, min(1.0, pow((difference(x, 0.0) / 30.0), 2.0)))) * (1.0 - getRiver(x, y, 4.0)) +
 			// usual ground noise
 		noise2(x * 0.1, y * 0.1) * 2.0 * (noise3(x * 0.01, y * 0.01) * 0.5 + 0.5) +
 			// finer ground noise
